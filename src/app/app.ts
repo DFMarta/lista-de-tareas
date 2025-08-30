2ce74200bb5f12026d88627360d3e81b0469ec3f
DFMarta <marta.dominguezfar@gmail.com>
2025-08-30 19:40:40 +0200

    Primer proyecto practicando Angular con una lista de tareas

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,32 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { Tareas } from './service/tareas';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [CommonModule, FormsModule],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
-export class App {
-  protected readonly title = signal('listaTareasApp');
+export class App implements OnInit {
+  listaTareas = signal<string[]>([]);
+  nuevaTarea: string = '';
+
+  private _tareaService = inject(Tareas);
+
+  ngOnInit(): void {
+    this.listaTareas.set(this._tareaService.getTareas());
+  }
+
+  agregarTarea() {
+    this._tareaService.agregarTarea(this.nuevaTarea);
+    this.listaTareas.set(this._tareaService.getTareas());
+    this.nuevaTarea = '';
+  }
+
+  eliminarTarea(index: number) {
+    this._tareaService.eliminarTarea(index);
+    this.listaTareas.set(this._tareaService.getTareas());
+  }
 }
